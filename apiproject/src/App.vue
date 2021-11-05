7cc2bb60adb03b31d1cf683d8d8184e5e6371793
ZhangTerrence <71456929+ZhangTerrence@users.noreply.github.com>
2021-11-05 12:26:47 -0400

    E

--- apiproject/src/App.vue
+++ apiproject/src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <section id="app">
+    <button v-on:click="hi"></button>
+  </section>
 </template>
 
+<script>
+export default {
+  name: "app",
+  methods: {
+    hi: function (){
+      try {
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+button{
+  width: 10rem;
 }
 </style>
