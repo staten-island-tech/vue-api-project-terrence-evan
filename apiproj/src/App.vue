6ebc3233d5dad3e1b0652d258ac0a7aa5a57627a
ZhangTerrence <71456929+ZhangTerrence@users.noreply.github.com>
2021-11-09 12:24:49 -0500

    LMAo

--- apiproj/src/App.vue
+++ apiproj/src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <nav id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link class="searchbar" to="/challenger">Leaderboard</router-link>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
-
+*{
+  font-size: 62.5%;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+#nav{
+  width: 100vw;
+  height: 5vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.searchbar{
+  padding-left: 3rem;
+}
+a{
+  width: 10rem;
+  font-size: 2.5rem;
+}
 </style>
