0ce106215838489ef71d56ea52c0fdb0af20aceb
ZhangTerrence <71456929+ZhangTerrence@users.noreply.github.com>
2021-11-09 12:14:43 -0500

    E

--- apiproj/src/App.vue
+++ apiproj/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link class="searchbar" to="/about">About</router-link>
     <router-view/>
   </div>
 </template>
