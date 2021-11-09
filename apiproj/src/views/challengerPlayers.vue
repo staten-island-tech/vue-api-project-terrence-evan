3f3e620d2bf2ab15a8b635cb9f4e390206c0e887
ZhangTerrence <71456929+ZhangTerrence@users.noreply.github.com>
2021-11-09 12:10:54 -0500

    E

--- apiproj/src/views/challengerPlayers.vue
+++ apiproj/src/views/challengerPlayers.vue
@@ -1,3 +1,34 @@
 <template>
-    <section>Hi</section>
+  <section id="challenger-display">
+      <div class="e" v-for="(challenger, id) in challengers" :key="id">{{challenger.summonerName}}</div>
+  </section>
 </template>
+
+<script>
+export default {
+    name: "challenger",
+    data() {
+        return {
+            challengers: []
+        }
+    },
+    created: function(){
+        this.fetchData()
+    },
+    methods: {
+        fetchData: async function(){
+            try {
+                const response = await fetch("https://na1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-eeea740b-536c-472c-8c1b-2a79c819f8d5")
+                const data = await response.json()
+                this.challengers = data.entries
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
