1c661a61fefdbdd179b4366f1db834d881f84e86
ZhangTerrence <71456929+ZhangTerrence@users.noreply.github.com>
2021-11-11 15:47:57 -0500

    Some things

--- apiproj/src/views/searchPlayers.vue
+++ apiproj/src/views/searchPlayers.vue
@@ -1,8 +1,11 @@
 <template>
   <section id="searchpage">
     <div id="searchcontainer">
-        <input id="searchbar" type="text" placeholder="Search..." v-on:keyup="test">
+        <input id="searchbar" type="text" placeholder="Search..." @keyup="test">
         <button class="button" type="submit"><i class="fas fa-search"></i></button>
+    </div>
+    <div id="infoPage" v-for="(content, id) in summonerInfo" :key="id">
+        <div>{{content}}</div>
     </div>
   </section>
 </template>
@@ -20,28 +23,18 @@ export default {
             if (e.key === "Enter"){
                 const searchbar = document.getElementById("searchbar")
                 const searchContainer = document.getElementById("searchcontainer")
-                const searchPage = document.getElementById("searchpage")
                 const requested = searchbar.value
+                const infoPage = document.getElementById("infopage")
                 try {
                     // Gets Data
                     const response = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${requested}?api_key=RGAPI-f03be3fa-6049-4f7b-8023-0d902a89652b`)
                     const jsonify = await response.json()
                     this.summonerInfo = jsonify
-                    console.log(jsonify);
-                    // Creates new page 
-                    if (searchPage.children.length === 1){
-                        // Makes new div to contain info
-                        searchContainer.style.width = "75rem"
-                        searchContainer.style.height = "4rem"
-                        searchContainer.style.marginTop = "2rem"
-                        searchContainer.style.top = "0"
-                        document.querySelector(".fas").style.fontSize = "2rem"
-                        searchPage.style.justifyContent = "space-between"
-                        const infoPage = document.createElement("div")
-                        infoPage.style.width = "100%"
-                        infoPage.style.height = "80vh"
-                        searchPage.appendChild(infoPage)
-                    }
+                    // Brings out infoPage and makes some adjustments
+                    infoPage.style.display = "flex"
+                    searchContainer.style.width = "75rem"
+                    searchContainer.style.height = "4rem"
+                    document.querySelector(".fas").style.fontSize = "2rem"
                 } catch (error) {
                     console.log(error);
                 }
@@ -62,10 +55,9 @@ export default {
 }
 #searchcontainer{
     width: 100rem;
-    height: 10rem;
+    height: 10vh;
     display: flex;
     position: relative;
-    top: -4rem;
 }
 #searchbar{
     width: 85%;
@@ -82,5 +74,11 @@ export default {
 }
 .fas{
     font-size: 4rem;
+}
+#infoPage{
+    display: none;
+    background-color: red;
+    height: 90vh;
+    width: 100%;
 }
 </style>
